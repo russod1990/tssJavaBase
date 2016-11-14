49c45d486ee666aaa87d9de7de0d8647455a08ae
tss <tss@debian-tss24>
2016-11-14 12:11:00 +0100

    creati esempi sulle varibili

--- src/lezione2/Main.java
+++ src/lezione2/Main.java
@@ -7,32 +7,40 @@ package lezione2;
 
 /**
  * Esempio di commenti in Java
- * 
+ *
  * @author tss
  */
 public class Main {
-/**
- * da questo metodo partirà l'esecuzione del programma
- * 
- * @param args 
- */
-    
-public static void main(String[] args) {
+
+    int colore; //variabile di istanza
+    private static int carattere; // variabile statica
+
+    /**
+     * da questo metodo partirà l'esecuzione del programma
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
         // commento su singola riga
-        
+
         /*
         commento
         su
         più
         righe
-        */
-        
+         */
         /**
          * commento multiriga per i metodi (funzionalità)
          */
-        
         System.out.println("Start esempio commenti ...");
+        int totale = 10; //variabile locale
+        if (totale > 0) {
+            int x = 5; //variabile locale
+        } else {
+            int x = 5; //varibaile locale
+        }
+        int x = 10; //variabile locale
+        System.out.println("" + x);
     }
- 
-    
+
 }
