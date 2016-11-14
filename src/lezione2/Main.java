855a85d492f69064e6cf2ba1a67d89042b89742e
tss <tss@debian-tss24>
2016-11-14 11:13:10 +0100

    creato package lezion2

---
+++ src/lezione2/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione2;
+
+/**
+ * Esempio di commenti in Java
+ * 
+ * @author tss
+ */
+public class Main {
+/**
+ * da questo metodo partirà l'esecuzione del programma
+ * 
+ * @param args 
+ */
+    
+public static void main(String[] args) {
+        // commento su singola riga
+        
+        /*
+        commento
+        su
+        più
+        righe
+        */
+        
+        /**
+         * commento multiriga per i metodi (funzionalità)
+         */
+        
+        System.out.println("Start esempio commenti ...");
+    }
+ 
+    
+}
