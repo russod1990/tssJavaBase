b9e3f694bd191766c21c16338f6c0038cd93b620
tss <tss@debian-tss24>
2016-12-14 15:15:29 +0100

    lezione 7

---
+++ src/lezione07/interfacce/MultiMedia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione07.interfacce;
+
+/**
+ *
+ * @author tss
+ */
+public interface MultiMedia {
+    
+    
+    public static final String PLAYER="CD_PLAYER";
+    
+    void play();
+    void stop();
+    void pause();
+    
+}
